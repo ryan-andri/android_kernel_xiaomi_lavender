1acc1081f7a534a389f0d67988749eb733db7dc9
Ryan Andri <ryan-andri@outlook.com>
2020-07-03 17:37:55 +0700

    power: smb-lib: refactor lct_set_prop_input_suspend()
    lct_set_prop_input_suspend() is used for sysfs power supply as
    charging_enabled and it used for smart charging for some rom
    which sometime lead charging not working after battery charging
    stoped by smart charging. refactor it to avoid the issue.
    
    Change-Id: Id19dfb0514d029274e6b0982e9ef1c7a8e4ae733

--- drivers/power/supply/qcom/qpnp-smb2.c
+++ drivers/power/supply/qcom/qpnp-smb2.c
@@ -1374,6 +1374,7 @@ static int smb2_batt_set_prop(struct power_supply *psy,
 	switch (prop) {
 #ifdef CONFIG_MACH_LONGCHEER
 	case POWER_SUPPLY_PROP_CHARGING_ENABLED:
+		chg->charging_enabled = val->intval;
 		rc = lct_set_prop_input_suspend(chg, val);
 		break;
 #endif
@@ -2986,7 +2987,7 @@ static int smb2_probe(struct platform_device *pdev)
 	fb_register_client(&chg->notifier);
 	INIT_WORK(&chg->fb_notify_work, thermal_fb_notifier_resume_work);
 #endif
-	chg->charging_enabled = true;
+	chg->charging_enabled = 1;
 #endif
 
 	pr_info("QPNP SMB2 probed successfully usb:present=%d type=%d batt:present = %d health = %d charge = %d\n",
